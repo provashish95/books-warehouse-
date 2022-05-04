217c643535cd0247c999e6cfb00b783993212ac6
Provashish Roy <provashishroy95@gmail.com>
2022-05-04 17:05:56 +0600

    added design of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,48 @@ import './Footer.css';
 const Footer = () => {
     let date = new Date().getFullYear();
     return (
-        <div className='footer-style p-5 text-center'>
-            <span><i className="fa-brands fa-google icon-style"></i></span>
-            <span ><i className="fa-brands fa-facebook icon-style"></i></span>
-            <span ><i className="fa-brands fa-twitter icon-style"></i></span>
-            <span ><i className="fa-brands fa-whatsapp icon-style"></i></span>
-            <span ><i className="fa-brands fa-github icon-style"></i></span>
-            <p className='text-center text-white text-muted mt-3'>&copy; Copyright {date} | Books Warehouse</p>
-            <p className='text-center text-white text-muted'> Gain knowledge with Warehouse</p>
-        </div>
+        <footer className='footer-style py-5'>
+            <div className="container ">
+                <div className="row g-3 justify-content-between py-5  ">
+                    <div className="col-sm-12 col-md-4 col-lg-4 ">
+                        <div className='footer-text'>
+                            <h6>ABOUT US</h6>
+                            <p>text-center Lorem ipsum dolor sit amet consectetur adipisicingext-center Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, illum.</p>
+                            <span><i className="fa-solid fa-envelope "></i> bookswarehouse@gmail.com</span>
+                            <p><i className="fa-solid fa-phone"></i> 01632544810</p>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-12 col-md-4 col-lg-4 ">
+                        <div className='footer-text'>
+                            <h6>ADDRESS</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aliquid.</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-4 col-lg-4 ">
+                        <div className='footer-text'>
+                            <h6 className='footer-text mx-3'>USEFUL LINK</h6>
+                            <span className='mt-4'>
+                                <i className="fa-brands fa-github icon-style"></i>
+                                <i className="fa-brands fa-facebook icon-style"></i>
+                                <i className="fa-brands fa-google icon-style"></i>
+                                <i className="fa-brands fa-whatsapp icon-style"></i>
+                                <i className="fa-brands fa-twitter icon-style"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <hr className='footer-text' />
+                <div className="row ">
+                    <div className='col-12 '>
+                        <div className="text-center footer-text">
+                            &copy; Copyright {date} | Books Warehouse
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
     );
 };
 
