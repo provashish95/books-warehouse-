ef82bdb9d4445c9075766ca25edf006a75f49b75
Provashish Roy <provashishroy95@gmail.com>
2022-05-05 13:05:20 +0600

    create user with firebase and verify email

--- src/components/Home/Login/Login.js
+++ src/components/Home/Login/Login.js
@@ -1,30 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Login.css';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 
 const Login = () => {
     const [agree, setAgree] = useState(false);
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [userError, setUserError] = useState(' ');
+    const [
+        createUserWithEmailAndPassword,
+        registerUser,
+        registerLoading,
+        registerError,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    let errorElement;
 
-    const handleSignIn = () => {
-        signInWithGoogle();
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        if (password.length < 6) {
+            setUserError('Password must be 6 character!');
+        } else if (password !== confirmPassword) {
+            setUserError('Password not matched');
+        } else {
+            setUserError('')
+            createUserWithEmailAndPassword(email, password)
+            event.target.reset();
+        }
     }
 
-    if (user) {
-        console.log(user.user.email, user.user.displayName);
+    if (registerError) {
+        errorElement = <p className='text-danger '>Error: {registerError?.message}</p>
     }
+
+    useEffect(() => {
+        if (registerUser) {
+            console.log(registerUser);
+        }
+    }, [registerUser]);
+
 
     const handleLogin = (event) => {
         event.preventDefault();
-        console.log('login');
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        signInWithEmailAndPassword(email, password);
         event.target.reset();
     }
-    const handleRegister = (event) => {
-        event.preventDefault();
-        console.log('Register');
-        event.target.reset();
-    }
+
+
+
 
     return (
         <div className="container">
@@ -39,19 +73,21 @@ const Login = () => {
                                         <input type="text" className="w-100 rounded input-style py-2 px-2" id="exampleInputName" placeholder='Your Name' required />
                                     </div>
                                     <div className="mb-3">
-                                        <input type="email" className="w-100 rounded input-style py-2 px-2" id="exampleInputEmail" placeholder='Your Email' required />
+                                        <input type="email" name="email" className="w-100 rounded input-style py-2 px-2" id="exampleInputEmail" placeholder='Your Email' required />
                                     </div>
                                     <div className="mb-3">
-                                        <input type="password" className="w-100 rounded input-style py-2 px-2" id="exampleInputPassword1" placeholder='Password' required />
+                                        <input type="password" name="password" className="w-100 rounded input-style py-2 px-2" id="exampleInputPassword1" placeholder='Password' required />
                                     </div>
                                     <div className="mb-3">
-                                        <input type="password" className="w-100 rounded input-style py-2 px-2" id="exampleInputConfirmPassword1" placeholder='Confirm Password' required />
+                                        <input type="password" name="confirmPassword" className="w-100 rounded input-style py-2 px-2" id="exampleInputConfirmPassword1" placeholder='Confirm Password' required />
                                     </div>
+                                    <p className="text-danger ">{userError || ''}</p>
                                     <div className="mb-3 form-check">
                                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                                         <label className="form-check-label text-color" htmlFor="exampleCheck1">Agree terms & condition</label>
                                     </div>
                                     <button type="submit" className='btn  btn-style mx-auto d-block'>Register</button>
+                                    {errorElement}
                                 </form>
                             </>
                             :
