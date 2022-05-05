c251f803f161eb0f1a30b4aa976f9019198d48d4
Provashish Roy <provashishroy95@gmail.com>
2022-05-05 23:23:29 +0600

    social login system complete

--- src/components/Home/SocialLogin/SocialLogin.js
+++ src/components/Home/SocialLogin/SocialLogin.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import React, { useEffect } from 'react';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const SocialLogin = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
+    let from = location.state?.from?.pathname || "/";
+    let errorElement;
+
+    useEffect(() => {
+        if (googleUser) {
+            navigate(from, { replace: true })
+        }
+    }, [googleUser]);
+
+    useEffect(() => {
+        if (githubUser) {
+            navigate(from, { replace: true })
+        }
+    }, [githubUser]);
+
+    if (googleLoading || githubLoading) {
+        return <Loading></Loading>
+    }
+
+    if (googleError || githubError) {
+        errorElement = <p className='text-danger'>Error: {googleError?.message} {githubError?.message}</p>
+    }
+
+
     return (
         <div>
             <div className='d-flex align-items-center my-2'>
@@ -11,12 +40,13 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2 text-color'>or</p>
                 <div style={{ height: '2px' }} className='footer-style w-50 rounded'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-style mx-auto d-block w-50 my-2'>
+                <button onClick={() => signInWithGoogle()} className='btn btn-style mx-auto d-block w-50 my-2'>
                     <span className='me-3'><i className="fa-brands fa-google"></i></span>
                     <span>Google Login </span>
                 </button>
-                <button className='btn btn-style mx-auto d-block w-50 my-2'>
+                <button onClick={() => signInWithGithub()} className='btn btn-style mx-auto d-block w-50 my-2'>
                     <span className='me-3'><i className="fa-brands fa-github"></i></span>
                     <span>Github Login</span>
                 </button>
