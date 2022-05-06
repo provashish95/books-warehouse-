3d7976ec10caa9db1b6bafce80c20e64e5d1ba49
Provashish Roy <provashishroy95@gmail.com>
2022-05-06 16:50:13 +0600

    load data on ui by id from database using server api

--- src/components/Home/Book/Book.js
+++ src/components/Home/Book/Book.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Book.css';
 
 const Book = ({ book }) => {
     const navigate = useNavigate();
-    console.log(book);
-    const handleBook = id => {
+    const { name, description, price, quantity, supplierName, _id } = book;
+
+    const navigateToInventory = id => {
         navigate(`/inventory/${id}`);
     }
     return (
         <div className="card mx-auto card-style  shadow-lg p-3 mb-5 bg-body rounded" style={{ width: '18rem' }}>
             <img src={book.img} className="card-img-top" alt="..." />
             <div className="card-body text-color">
-                <h5 className="card-title">{book.name}</h5>
-                <p className="card-text">{book.description}</p>
-                <p className="card-text">Price: {book.price}</p>
-                <p className="card-text">Quantity: {book.quantity}</p>
-                <p className="card-text">Supplier Name: {book.supplierName}</p>
-                <button onClick={() => handleBook(book._id)} className='btn btn-style d-block mx-auto'>Manage</button>
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text">Price: {price}</p>
+                <p className="card-text">Quantity: {quantity}</p>
+                <p className="card-text">Supplier Name: {supplierName}</p>
+                <button onClick={() => navigateToInventory(_id)} className='btn btn-style d-block mx-auto'>Manage</button>
             </div>
         </div>
     );
--- src/components/Home/Inventories/Inventories.js
+++ src/components/Home/Inventories/Inventories.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const Inventories = () => {
+    const { id } = useParams();
+    const [book, setBook] = useState({});
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const url = `http://localhost:5000/book/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBook(data))
+    }, [id]);
+
+    if (Object.keys(book).length === 0) {
+        return <Loading></Loading>
+    }
+    console.log(book);
 
     return (
         <div className='container'>
-            <h1>Inventories</h1>
+            <h1>{book.name}</h1>
+            <img src={book.img} alt="img" style={{ width: '300px' }} />
+            <p>{book.description}</p>
+            <p>Price: {book.price}</p>
+            <p>Quantity: {book.quantity}</p>
+            <p>Supplier Name: {book.supplierName}</p>
+
             <p>display detailed item information like id, name, image, description, price, quantity, supplier name, sold, etc. This page will have a Button named delivered. Once this button is clicked, it will reduce the quantity of the item by one. Make sure this data is saved on the database, and the change in quantity is displayed on the website.</p>
             <p>There will be a small form on the inventory/:id page. The from will say, restock the items. You will have an input field where you can put some numbers. And hitting the button will increase the quantity of the item.</p>
 
